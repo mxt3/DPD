ec218e0752f050e54100021033e8f310e8761a20
Maxime Feyerick <maxime.feyerick@telenet.be>
2018-11-24 18:13:58 +0100

    Week 1 assignment

--- Week1/C/palindrome.c
+++ Week1/C/palindrome.c
@@ -1,21 +1,42 @@
+/*!
+ * \file
+ * \brief A simple program for checking if a word is a palindrome
+ *
+ * Call this program using as only argument the word to check.
+ * E.g:
+ *	> ./palindrome madam
+ */
+
 #include <stdio.h>
 #include <string.h>
 
-int pal(char a[]);
+int isPalindrome(char str[]);
 
+/*!
+ * \brief Entry of program
+ */
 int main(int argc, char *argv[]) {
     printf("%s is %s palindrome\n",
-           argv[1], pal(argv[1]) ? "a" : "not a");
+           argv[1], isPalindrome(argv[1]) ? "a" : "not a");
     return 0;
 }
 
-int pal(char a[]) {
-    if (strlen(a))
-        if (a[0] != a[strlen(a)])
+/*!
+ * \param str[] C-string to check
+ * \return 1 if str[] represents a palindrome
+ *
+ * The function calls itself recursively on substrings.
+ * An empty string is considered a palindrome.
+ */
+int isPalindrome(char str[]) {
+    if (strlen(str))
+		// check first and last character
+        if (str[0] != str[strlen(str)])
             return 0;
         else {
-            a[strlen(a)] = "\0";
-            return pal(a[1]);
+			// call again on substring
+            str[strlen(str)] = "\0";
+            return isPalindrome(str[1]);
         }
     else
         return 1;
